0f0c7bf843b7a54387bfea3994c64192b9492bd6
Vinicius Souza <viniciussouzaesp@gmail.com>
2024-12-15 06:44:41 -0300

    Criação da lógica de classificação de herói

---
+++ index.js
@@ -0,0 +1,43 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("Digite o nome do herói: ", (nome) => {
+  rl.question(
+    "Digite a quantidade de experiência (XP) do herói: ",
+    (inputXp) => {
+      let xp = parseInt(inputXp);
+
+      if (isNaN(xp)) {
+        console.error("Por favor, insira um número válido para XP.");
+        rl.close();
+        return;
+      }
+
+      let nivel;
+      if (xp < 1000) {
+        nivel = "Ferro";
+      } else if (xp >= 1001 && xp <= 2000) {
+        nivel = "Bronze";
+      } else if (xp >= 2001 && xp <= 5000) {
+        nivel = "Prata";
+      } else if (xp >= 5001 && xp <= 7000) {
+        nivel = "Ouro";
+      } else if (xp >= 7001 && xp <= 8000) {
+        nivel = "Platina";
+      } else if (xp >= 8001 && xp <= 9000) {
+        nivel = "Ascendente";
+      } else if (xp >= 9001 && xp <= 10000) {
+        nivel = "Imortal";
+      } else {
+        nivel = "Radiante";
+      }
+
+      console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
+      rl.close();
+    }
+  );
+});
